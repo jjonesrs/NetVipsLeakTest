278e343f888649f8f11e45ab1af07697f3cf0088
Josh <joshua.jones@repspark.com>
2019-01-18 11:23:01 -0800

    Initial commit

---
+++ NetVipsLeakTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using NetVips;
+
+namespace NetVipsLeakTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] imageBytes = File.ReadAllBytes(@"large-test-image.png");
+            //byte[] imageBytes = File.ReadAllBytes(@"small-test-image.jpg");
+
+            Base.LeakSet(1);
+
+            Operation.VipsCacheSetMax(0);
+            Operation.VipsCacheSetMaxFiles(0);
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                using (Image img = Image.NewFromBuffer(imageBytes))
+                {
+                    Console.WriteLine(img);
+                }
+            }
+        }
+    }
+}
